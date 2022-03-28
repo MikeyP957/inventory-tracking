62dc61ef6e17cf3f587f38b70aff5e815a11629d
MikeyP957 <peraram2@gmail.com>
2022-03-28 15:46:34 -0700

    change sass to scss

--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { StockInventoryComponent } from "./containers/stock-inventory/stock-inventory.component";
+
 import { StockBranchComponent } from "./components/stock-branch/stock-branch.component";
 import { StockProductsComponent } from "./components/stock-products/stock-products.component";
 import { StockSelectorComponent } from "./components/stock-selector/stock-selector.component";
